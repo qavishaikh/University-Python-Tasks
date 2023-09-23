bc51e4f694449014030d8792d0990b118f687084
Qavi <qavi.shaikh11@gmail.com>
2023-09-23 10:37:47 +0500

    Replace List -2

--- Replace.py
+++ Replace.py
@@ -9,3 +9,20 @@ list1 = ["A","B","C"]
 a = list1.index("B")
 list1[a] = "Q"
 print(list1)
+
+print("\n Example 3 ")
+list2 = ["a","b","c","d"]
+index = 0
+while index < len(list2):
+    if list2[index] == "a":
+        list2[index] =  "q"
+    index += 1
+print(list2)
+
+print("\n Example 4 ")
+list3 = ["A","B","C","D","E"]
+for index,value in enumerate(list3):
+    if value == "D":
+        list3[index] = "Q"
+print(list3)
+
