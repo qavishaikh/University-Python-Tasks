0e4e9d133c3014483e2609c9439497695b2cca26
Qavi <qavi.shaikh11@gmail.com>
2023-09-23 10:19:28 +0500

    Replace List Methods -1

---
+++ Replace.py
@@ -0,0 +1,11 @@
+# Replace Methods in List
+print("\n Example 1")
+list = ["1","2","3","4","5"]
+list[1] = "8"
+print(list)
+
+print("\n Example 2 ")
+list1 = ["A","B","C"]
+a = list1.index("B")
+list1[a] = "Q"
+print(list1)
